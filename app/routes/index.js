23840c00790bb2b6b66a80f9a6fde3ce7a4cb765
jingws <632397482@qq.com>
2019-07-15 15:38:55 +0800

    推荐歌单

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,13 @@
 import Router from 'koa-router'
 import banner from './banner'
-import recommendHot from './recommend-hot'
-import playListInfo from './playListInfo'
-import playListDetail from './playListDetail'
-import songLyric from './songLyric'
+import recommend from './recommend'
+import playListDetail from './playList_detail'
+import songLyric from './song_lyric'
 
 const router = Router()
 
 router.use(banner.routes(), banner.allowedMethods())
-router.use(recommendHot.routes(), recommendHot.allowedMethods())
-router.use(playListInfo.routes(), playListInfo.allowedMethods())
+router.use(recommend.routes(), recommend.allowedMethods())
 router.use(playListDetail.routes(), playListDetail.allowedMethods())
 router.use(songLyric.routes(), songLyric.allowedMethods())
 
--- app/routes/urlConfig.js
+++ app/routes/urlConfig.js
@@ -6,3 +6,6 @@ export const discover = 'https://music.163.com/discover'
 
 // banner
 export const banner = 'https://music.163.com/api/banner/get'
+
+// 推荐歌单
+export const recommend = 'https://music.163.com/api/personalized/playlist'
