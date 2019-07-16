88ccbb11b563a015b94bc54bd360c2374764ff73
jingws <632397482@qq.com>
2019-07-16 15:27:48 +0800

    评论增加每页获取数量字段

--- app/routes/comment_music.js
+++ app/routes/comment_music.js
@@ -8,9 +8,10 @@ const router = Router()
 router.post('/comment-music', async (ctx, next) => {
   const id = ctx.request.body.id
   const offset = ctx.request.body.offset ? ctx.request.body.offset : 0
+  const limit = ctx.request.body.limit ? ctx.request.body.limit : 20
 
   const data = await new Promise(resolve => {
-    request.get(commentsMusic + id + '?offset=' + offset)
+    request.get(commentsMusic + id + '?offset=' + offset + '?limit=' + limit)
       .end((err, res) => {
         const r = res.text
         resolve(r)
